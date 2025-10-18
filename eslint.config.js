643eae5bc356096ca035ffb4d6bcb762a38454c6
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-18 19:14:17 -0300

    fix(eslint): corrige a configuração

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,48 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    files: ['**/*.js', '**/*.jsx'],
+    plugins: { js, react, 'react-hooks': reactHooks },
+    extends: ['js/recommended'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off'
     },
+    settings: {
+      react: { version: 'detect' }
+    }
   },
-])
+  {
+    files: ['**/*.ts'],
+    plugins: { '@typescript-eslint': ts, react, 'react-hooks': reactHooks },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' }
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      semi: ['error', 'always'],
+      'max-len': [
+        'error',
+        {
+          code: 80,
+          ignoreUrls: false,
+          ignoreComments: false,
+          ignoreStrings: false,
+          ignoreTemplateLiterals: false
+        }
+      ],
+      curly: ['error', 'all'],
+      quotes: ['error', 'single', { avoidEscape: true }]
+    }
+  }
+]);
