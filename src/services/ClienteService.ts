616932d1aff7c2c5b56e6558fa86446e3432d471
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-19 20:17:23 -0300

    refactor(services): troca as arrow functions por functions em cliente service

--- src/services/ClienteService.ts
+++ src/services/ClienteService.ts
@@ -2,7 +2,7 @@ import api from './api';
 import type { Cliente } from '../models/Cliente';
 
 // Função para buscar todos os clientes
-export const getClientes = async (): Promise<Cliente[]> => {
+export async function getClientes(): Promise<Cliente[]> {
   try {
     const response = await api.get<Cliente[]>('/clientes/');
     return response.data;
@@ -10,10 +10,10 @@ export const getClientes = async (): Promise<Cliente[]> => {
     console.error('Erro ao buscar clientes:', error);
     throw error;
   }
-};
+}
 
 // Função para criar um cliente
-export const createCliente = async (cliente: Cliente): Promise<Cliente> => {
+export async function createCliente(cliente: Cliente): Promise<Cliente> {
   try {
     const response = await api.post<Cliente>('/clientes/', cliente);
     return response.data;
@@ -21,13 +21,13 @@ export const createCliente = async (cliente: Cliente): Promise<Cliente> => {
     console.error('Erro ao criar cliente:', error);
     throw error;
   }
-};
+}
 
 // Função para atualizar um cliente
-export const updateCliente = async (
+export async function updateCliente(
   id: number,
   clienteAtualizado: Cliente
-): Promise<Cliente> => {
+): Promise<Cliente> {
   try {
     const response = await api.put<Cliente>(
       `/clientes/${id}/`,
@@ -38,14 +38,14 @@ export const updateCliente = async (
     console.error('Erro ao atualizar cliente:', error);
     throw error;
   }
-};
+}
 
 // Função para deletar um cliente
-export const deleteCliente = async (id: number) => {
+export async function deleteCliente(id: number) {
   try {
     await api.delete<Cliente>(`/clientes/${id}`);
   } catch (error) {
     console.error('Erro ao deletar cliente:', error);
     throw error;
   }
-};
+}
