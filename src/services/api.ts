310b555c6483a62494338f5f18e7bdee76da093a
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-19 20:23:11 -0300

    feat(api): adiciona interceptor para autenticação em cada request do axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,4 +7,15 @@ const api = axios.create({
   }
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('access_token');
+    if (accessToken && config.headers) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default api;
