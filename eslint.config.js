3e967db0a0d9b90c64b72b5f0bc020c39981f6f6
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-19 14:55:21 -0300

    build(eslint): simplifica as configurações e adiciona novos defaults

--- eslint.config.js
+++ eslint.config.js
@@ -1,48 +1,29 @@
-import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
-import ts from '@typescript-eslint/eslint-plugin';
-import react from 'eslint-plugin-react';
+import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
+  globalIgnores(['dist']),
   {
-    files: ['**/*.js', '**/*.jsx'],
-    plugins: { js, react, 'react-hooks': reactHooks },
-    extends: ['js/recommended'],
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-undef': 'warn',
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off'
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
     },
-    settings: {
-      react: { version: 'detect' }
-    }
-  },
-  {
-    files: ['**/*.ts'],
-    plugins: { '@typescript-eslint': ts, react, 'react-hooks': reactHooks },
     rules: {
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        { argsIgnorePattern: '^_' }
-      ],
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
       semi: ['error', 'always'],
-      'max-len': [
-        'error',
-        {
-          code: 80,
-          ignoreUrls: false,
-          ignoreComments: false,
-          ignoreStrings: false,
-          ignoreTemplateLiterals: false
-        }
-      ],
       curly: ['error', 'all'],
-      quotes: ['error', 'single', { avoidEscape: true }]
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'max-len': ['error', { code: 80 }]
     }
   }
 ]);
