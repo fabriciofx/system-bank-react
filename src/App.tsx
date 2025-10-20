d0121c9a48b416908979f75ef41241a7317b9e85
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-20 13:24:11 -0300

    feat(pages): adiciona FormLogin

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Clientes from './pages/Clientes';
+import FormLogin from './pages/FormLogin';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Clientes />} />
+        <Route path="/" element={<FormLogin />} />
         <Route path="/clientes" element={<Clientes />} />
       </Routes>
     </Router>
---
+++ src/pages/FormLogin.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, styled, TextField } from '@mui/material';
+import type { Credentials } from '../models/Credentials';
+import { login } from '../services/AuthService';
+
+const FormBox = styled('form')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.spacing(2),
+  width: 400,
+  padding: theme.spacing(2)
+}));
+
+export default function FormLogin() {
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState<Credentials>({
+    username: '',
+    password: ''
+  });
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    setCredentials({ ...credentials, [name]: value });
+  }
+
+  async function handleSubmit(
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+    await login(credentials, navigate);
+  }
+
+  return (
+    <FormBox onSubmit={handleSubmit}>
+      <TextField
+        label="E-mail ou Nome de Usuário"
+        id="username"
+        name="username"
+        variant="filled"
+        value={credentials.username}
+        onChange={handleChange}
+        required
+      />
+      <TextField
+        type="password"
+        label="password"
+        id="password"
+        name="password"
+        variant="filled"
+        value={credentials.password}
+        onChange={handleChange}
+        required
+      />
+      <Button type="submit">Enviar</Button>
+    </FormBox>
+  );
+}
