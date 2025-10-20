a436317a83fcaf0d76380b31a4d7ee3c0943691f
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-20 15:08:01 -0300

    fix: remove o withCredentials que estava impedindo o login

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,8 +5,7 @@ const api = axios.create({
   baseURL: 'https://aula-angular.bcorp.tec.br/api',
   headers: {
     'Content-Type': 'application/json'
-  },
-  withCredentials: true
+  }
 });
 
 api.interceptors.request.use((config) => {
@@ -25,11 +24,7 @@ api.interceptors.response.use(
       config._retry = true;
       try {
         const refreshToken = STORAGE.value('refresh_token')[0] ?? '';
-        const refreshResponse = await api.post(
-          '/token/refresh/',
-          refreshToken,
-          { withCredentials: true }
-        );
+        const refreshResponse = await api.post('/token/refresh/', refreshToken);
         STORAGE.store('access_token', refreshResponse.data.access);
         config.headers!.Authorization = `Bearer ${refreshResponse.data.access}`;
         return api(config);
