f8af386dc96a81b1c7f76b76a2196ffef8371031
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-10-22 11:21:29 -0300

    feat(pages): adiciona a página de login template

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Clientes from './pages/Clientes';
-import FormLogin from './pages/FormLogin';
 import NovoCliente from './pages/NovoCliente';
+import LoginTemplate from './pages/LoginTemplate';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<FormLogin />} />
+        <Route path="/" element={<LoginTemplate />} />
         <Route path="/clientes" element={<Clientes />} />
         <Route path="/clientes/novo" element={<NovoCliente />} />
       </Routes>
---
+++ src/pages/LoginTemplate.tsx
@@ -0,0 +1,20 @@
+import FormLogin from '../components/FormLogin';
+import './LoginTemplate.css';
+
+function LoginTemplate() {
+  return (
+    <div className="container">
+      <div className="form">
+        <div className="logo">
+          <span className="title">SystemBank</span>
+          <img src="assets/logo.png" width="150px" alt="SystemBank logo" />
+        </div>
+        <div className="login-form">
+          <FormLogin></FormLogin>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginTemplate;
