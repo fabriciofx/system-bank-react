b558c8ad7b86213051c75d49c838b283724d25de
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-04 20:56:39 -0300

    build(test): configura o vitest a executar no modo headless por default

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     browser: {
+      headless: true,
       enabled: true,
       provider: playwright(),
       instances: [{ browser: 'chromium' }]
