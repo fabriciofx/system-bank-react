dd568df2fd3275d24b7abf1222c21a1b81e10a40
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-04 20:48:02 -0300

    build(test): configura os testes para usar o vitest + playwright

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,14 @@
 import react from '@vitejs/plugin-react';
+import { playwright } from '@vitest/browser-playwright';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./vitest.setup.ts']
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      instances: [{ browser: 'chromium' }]
+    }
   }
 });
