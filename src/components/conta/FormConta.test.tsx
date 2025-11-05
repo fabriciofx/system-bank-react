6f1e4a0bb7a68ce3789f37f4102c49cba280e887
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-05 14:29:38 -0300

    test(conta): adiciona teste para preencher o formulário de nova conta

--- src/components/conta/FormConta.test.tsx
+++ src/components/conta/FormConta.test.tsx
@@ -28,4 +28,27 @@ describe('Componente FormConta', () => {
     );
     await expect.element(getByRole('button')).toHaveTextContent('Criar');
   });
+
+  it('preenche e envia o formulário com sucesso', async () => {
+    const { getByRole, getByLabelText } = await render(
+      <MemoryRouter>
+        <FormConta
+          create={createContaFake}
+          update={updateContaFake}
+          findById={contaByIdFake}
+          pages={pagesClientesFake}
+          clienteById={clienteByIdFake}
+          buttonText="Cadastrar"
+        />
+      </MemoryRouter>
+    );
+    await getByRole('combobox').click();
+    await getByRole('option').nth(1).click();
+    await getByLabelText(/número/i).fill('12345');
+    await getByLabelText(/agência/i).fill('1234');
+    await getByLabelText(/saldo/i).fill('1000');
+    await getByRole('button').click();
+    const title = document.getElementsByClassName('swal2-title')[0];
+    expect(title.textContent).toEqual('Sucesso!');
+  });
 });
