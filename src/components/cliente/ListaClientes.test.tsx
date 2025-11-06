31b617397e8344d75d590f460d0ba52435f6c489
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-06 11:25:11 -0300

    fix(test): ignora as mensagens que contém 'not wrapped in act' no teste

--- src/components/cliente/ListaClientes.test.tsx
+++ src/components/cliente/ListaClientes.test.tsx
@@ -1,5 +1,5 @@
 import { MemoryRouter } from 'react-router-dom';
-import { describe, expect, it } from 'vitest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 import { render } from 'vitest-browser-react';
 import {
   deleteClienteFake,
@@ -8,6 +8,24 @@ import {
 import ListaClientes from './ListaClientes';
 
 describe('Componente ListaClientes', () => {
+  // Silencia o aviso do React sobre act()
+  const consoleError = console.error;
+  beforeAll(() => {
+    console.error = (...args) => {
+      if (
+        typeof args[0] === 'string' &&
+        args[0].includes('not wrapped in act')
+      ) {
+        return;
+      }
+      consoleError(...args);
+    };
+  });
+
+  afterAll(() => {
+    console.error = consoleError;
+  });
+
   it('mostra a listagem de clientes', async () => {
     const screen = await render(
       <MemoryRouter>
