aa508aaa05f86ab9b222dee3201a83a1e4007404
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-07 11:15:01 -0300

    test(login): adiciona teste para o FormLogin

---
+++ src/components/login-form/FormLogin.test.tsx
@@ -0,0 +1,20 @@
+import { MemoryRouter, type NavigateFunction } from 'react-router-dom';
+import { describe, expect, it, vi } from 'vitest';
+import { render } from 'vitest-browser-react';
+import { loginFake } from '../../services/AuthServiceFake';
+import FormLogin from './FormLogin';
+
+describe('FormLogin', () => {
+  it('deve autenticar o usuário', async () => {
+    const fakeNavigate = vi.fn() as unknown as NavigateFunction;
+    const screen = await render(
+      <MemoryRouter>
+        <FormLogin login={loginFake} navigate={fakeNavigate} />
+      </MemoryRouter>
+    );
+    await screen.getByLabelText(/e-mail ou nome de usuário/i).fill('teste');
+    await screen.getByLabelText(/senha/i).fill('12345678');
+    await screen.getByRole('button').click();
+    expect(fakeNavigate).toHaveBeenCalledWith('/clientes');
+  });
+});
