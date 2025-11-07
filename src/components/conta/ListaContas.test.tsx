609f91b58e938157ac27df4987fcc32f1b617072
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-06 21:25:36 -0300

    test(conta): adiciona teste de listagem de contas

---
+++ src/components/conta/ListaContas.test.tsx
@@ -0,0 +1,39 @@
+import { MemoryRouter } from 'react-router-dom';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { render } from 'vitest-browser-react';
+import { pagesContasClientesFakeFixed } from '../../services/ContaClienteServiceFake';
+import { deleteContaFake } from '../../services/ContaServiceFake';
+import ListaContas from './ListaContas';
+
+describe('ListaContas', () => {
+  // Silencia o aviso do React sobre act()
+  const consoleError = console.error;
+  beforeAll(() => {
+    console.error = (...args) => {
+      if (
+        typeof args[0] === 'string' &&
+        args[0].includes('not wrapped in act')
+      ) {
+        return;
+      }
+      consoleError(...args);
+    };
+  });
+
+  afterAll(() => {
+    console.error = consoleError;
+  });
+
+  it('deve mostrar a listagem de clientes', async () => {
+    const screen = await render(
+      <MemoryRouter>
+        <ListaContas
+          pages={pagesContasClientesFakeFixed}
+          remove={deleteContaFake}
+        />
+      </MemoryRouter>
+    );
+    const rows = screen.container.querySelectorAll('tr');
+    expect(rows.length).toBeGreaterThan(5);
+  });
+});
