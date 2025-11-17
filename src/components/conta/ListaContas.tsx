b96413689629c3cd1ceab78305f57c1a474f4e3a
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-17 15:22:26 -0300

    fix(conta): adiciona o objeto error em remove da listagem de contas

--- src/components/conta/ListaContas.tsx
+++ src/components/conta/ListaContas.tsx
@@ -27,7 +27,7 @@ type ListaContasProps = {
   ) => UseQueryResult<PageResult<ContaCliente>, Error>;
   remove: (options: {
     onSuccess: () => void;
-    onError: () => void;
+    onError: (error: Error) => void;
   }) => UseMutationResult<void, Error, Id, unknown>;
   rowsPage: number;
 };
@@ -44,8 +44,11 @@ export default function ListaContas({
   const deleta = remove({
     onSuccess: async () =>
       await new SuccessMessage('Sucesso!', 'Conta apagada com sucesso!').show(),
-    onError: async () =>
-      await new ErrorMessage('Oops!', 'Erro ao deletar a conta!').show()
+    onError: async (error: Error) =>
+      await new ErrorMessage(
+        'Oops!',
+        `Erro ao deletar a conta: ${error.message}`
+      ).show()
   });
 
   function handleEdit(contaCliente: ContaCliente): void {
