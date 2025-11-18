70101a7f69118c6aebf03d3851f811ad9b5719ef
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-18 14:05:50 -0300

    fix(saque): corrige chamada do form de saque

--- src/pages/conta/SaquePage.tsx
+++ src/pages/conta/SaquePage.tsx
@@ -1,6 +1,9 @@
+import { useNavigate } from 'react-router-dom';
 import SaqueForm from '../../components/conta/SaqueForm';
 import Navbar from '../../components/navbar/Navbar';
 import { useSaque } from '../../hooks/useSaque';
+import { pagesClientes } from '../../services/ClienteService';
+import { listContas } from '../../services/ContaService';
 import './conta.css';
 
 export default function SaquePage() {
@@ -10,7 +13,12 @@ export default function SaquePage() {
       <div className="box-com-titulo">
         <div className="box-conta">
           <h1>Saque</h1>
-          <SaqueForm withdrawal={useSaque} />
+          <SaqueForm
+            withdrawal={useSaque}
+            clientes={pagesClientes}
+            contas={listContas}
+            navigate={useNavigate()}
+          />
         </div>
       </div>
     </div>
