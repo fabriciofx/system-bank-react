8c5b183f526adf3f22d81e3ba8bb7aec0c613833
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-12-12 11:17:29 -0300

    fix: corrige retorno do fakeContaById que devia retorna um Conta[]

--- src/services/FakeContaService.ts
+++ src/services/FakeContaService.ts
@@ -20,12 +20,14 @@ export const fakeUpdateConta = async (
   saldo: conta.saldo
 });
 
-export const fakeContaById = async (id: number): Promise<Conta> => ({
-  id: id,
-  cliente: randomInt(1, 100),
-  numero: String(randomInt(10000, 99999)),
-  agencia: String(randomInt(1000, 9999)),
-  saldo: String(randomInt(10, 10000))
-});
+export const fakeContaById = async (id: number): Promise<Conta[]> => [
+  {
+    id: id,
+    cliente: randomInt(1, 100),
+    numero: String(randomInt(10000, 99999)),
+    agencia: String(randomInt(1000, 9999)),
+    saldo: String(randomInt(10, 10000))
+  }
+];
 
 export const fakeDeleteConta = async (_id: number): Promise<void> => {};
